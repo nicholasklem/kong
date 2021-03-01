1ebdce82b85164d5903fdb327bf94f0689546be4
Enrique García Cota <kikito@gmail.com>
2021-03-01 17:25:00 +0100

    fix(declarative) topological sort prioritizes core in declarative config
    This fix to the declarative configuration changes the way entities are
    sorted:
    
    * Prioritizes core entities over plugin entities - this avoid problems
      when plugin entities use core entities but don't explicitly depend on them.
    * It additionally sorts entities alphabetically in order to provide a
      more consistent sorting algorithm - two lists of the same schemas in
      different orders will yield the same result.
    * The entities are still topologically sorted, the previous two changes
      are supedited to that.
    
    Note that the previous "special treatment of workspaces" is now part of the "prioritize core over plugin entities" code.

--- kong/db/declarative/init.lua
+++ kong/db/declarative/init.lua
@@ -1,11 +1,11 @@
 local declarative_config = require "kong.db.schema.others.declarative_config"
-local topological_sort = require "kong.db.schema.topological_sort"
 local workspaces = require "kong.workspaces"
 local pl_file = require "pl.file"
 local lyaml = require "lyaml"
 local cjson = require "cjson.safe"
 local tablex = require "pl.tablex"
 local constants = require "kong.constants"
+local utils = require "kong.tools.utils"
 
 
 local deepcopy = tablex.deepcopy
@@ -13,17 +13,94 @@ local null = ngx.null
 local SHADOW = true
 local md5 = ngx.md5
 local pairs = pairs
+local utils_toposort = utils.topological_sort
 local ngx_socket_tcp = ngx.socket.tcp
 local REMOVE_FIRST_LINE_PATTERN = "^[^\n]+\n(.+)$"
 local PREFIX = ngx.config.prefix()
 local SUBSYS = ngx.config.subsystem
 local WORKER_COUNT = ngx.worker.count()
 local DECLARATIVE_HASH_KEY = constants.DECLARATIVE_HASH_KEY
+local CORE_ENTITIES = constants.CORE_ENTITIES
 
 local declarative = {}
 
 
 local Config = {}
+
+
+
+
+-- Given an array of schemas, return a copy of it sorted so that:
+--
+-- * If schema B has a foreign key to A, then B appears after A
+-- * When there's no foreign keys, core schemas appear before plugin entities
+-- * If none of the rules above apply, schemas are sorted alphabetically by name
+--
+-- The function returns an error if cycles are found in the schemas
+-- (i.e. A has a foreign key to B and B to A)
+--
+-- @tparam array schemas an array with zero or more schemas
+-- @treturn array|nil an array of schemas sorted topologically, or nil if cycle was found
+-- @treturn nil|string nil if the schemas were sorted, or a message if a cycle was found
+-- @usage
+-- local res = topological_sort({ services, routes, plugins, consumers })
+-- assert.same({ consumers, services, routes, plugins }, res)
+local schemas_topological_sort do
+
+  local CORE_SCORE = {}
+  for _, v in ipairs(CORE_ENTITIES) do
+    CORE_SCORE[v] = 1
+  end
+  CORE_SCORE["workspaces"] = 2
+
+
+  local function sort_core_first(a, b)
+    local sa = CORE_SCORE[a.name] or 0
+    local sb = CORE_SCORE[b.name] or 0
+    if sa == sb then
+      return a.name < b.name
+    end
+    return sa < sb
+  end
+
+  schemas_topological_sort = function(schemas)
+    local s
+    local schemas_by_name = {}
+    local copy = {}
+
+    for i = 1, #schemas do
+      s = schemas[i]
+      schemas_by_name[s.name] = s
+      copy[i] = schemas[i]
+    end
+    schemas = copy
+
+    table.sort(schemas, sort_core_first)
+
+    -- given a schema, return all the schemas to which it has references
+    -- (and are in the list of the `schemas` provided)
+    local get_schema_neighbors = function(schema)
+      local neighbors = {}
+      local neighbors_len = 0
+      local neighbor
+
+      for _, field in schema:each_field() do
+        if field.type == "foreign"  then
+          neighbor = schemas_by_name[field.reference] -- services
+          if neighbor then
+            neighbors_len = neighbors_len + 1
+            neighbors[neighbors_len] = neighbor
+          end
+          -- else the neighbor points to an unknown/uninteresting schema. This happens in tests.
+        end
+      end
+
+      return neighbors
+    end
+
+    return utils_toposort(schemas, get_schema_neighbors)
+  end
+end
 
 
 -- Produce an instance of the declarative config schema, tailored for a
@@ -336,7 +413,7 @@ function declarative.load_into_db(entities, meta)
       return nil, "unknown entity: " .. entity_name
     end
   end
-  local sorted_schemas, err = topological_sort(schemas)
+  local sorted_schemas, err = schemas_topological_sort(schemas)
   if not sorted_schemas then
     return nil, err
   end
@@ -377,7 +454,7 @@ local function export_from_db(emitter, skip_ws)
       table.insert(schemas, dao.schema)
     end
   end
-  local sorted_schemas, err = topological_sort(schemas)
+  local sorted_schemas, err = schemas_topological_sort(schemas)
   if not sorted_schemas then
     return nil, err
   end
@@ -901,6 +978,9 @@ function declarative.sanitize_output(entities)
     end
   end
 end
+
+-- exposed for tests only
+declarative._schemas_topological_sort = schemas_topological_sort
 
 
 return declarative
--- spec/01-unit/01-db/10-declarative_spec.lua
+++ spec/01-unit/01-db/10-declarative_spec.lua
@@ -1,7 +1,7 @@
 require("spec.helpers") -- for kong.log
 local declarative = require "kong.db.declarative"
 local conf_loader = require "kong.conf_loader"
-
+local Schema = require "kong.db.schema"
 
 local null = ngx.null
 
@@ -45,5 +45,131 @@ keyauth_credentials:
     assert.equal(nil, err)
 
     assert.is_nil(entities.keyauth_credentials['3f9066ef-b91b-4d1d-a05a-28619401c1ad'].ttl)
+  end)
+
+
+  describe("schemas_topological_sort", function()
+
+    local function collect_names(schemas)
+      local names = {}
+      for i = 1, #schemas do
+        names[i] = schemas[i].name
+      end
+      return names
+    end
+
+
+    local function schema_new(s)
+      return assert(Schema.new(s))
+    end
+
+    local ts = declarative._schemas_topological_sort
+
+    it("sorts an array of unrelated schemas alphabetically by name", function()
+      local a = schema_new({ name = "a", fields = { { a = { type = "string" } } } })
+      local b = schema_new({ name = "b", fields = { { b = { type = "boolean" } } } })
+      local c = schema_new({ name = "c", fields = { { c = { type = "integer" } } } })
+
+      local x = ts({ a, b, c })
+      assert.same({"c", "b", "a"},  collect_names(x))
+    end)
+
+    it("it puts destinations first", function()
+      local a = schema_new({ name = "a", fields = { { a = { type = "string" } } } })
+      local c = schema_new({
+        name = "c",
+        fields = {
+          { c = { type = "integer" }, },
+          { a = { type = "foreign", reference = "a" }, },
+        }
+      })
+      local b = schema_new({
+        name = "b",
+        fields = {
+          { b = { type = "boolean" }, },
+          { a = { type = "foreign", reference = "a" }, },
+          { c = { type = "foreign", reference = "c" }, },
+        }
+      })
+
+      local x = ts({ a, b, c })
+      assert.same({"a", "c", "b"},  collect_names(x))
+    end)
+
+    it("puts core entities first, even when no relations", function()
+      local a = schema_new({ name = "a", fields = { { a = { type = "string" } } } })
+      local routes = schema_new({ name = "routes", fields = { { c = { type = "boolean" } } } })
+
+      local x = ts({ a, routes })
+      assert.same({"routes", "a"},  collect_names(x))
+    end)
+
+    it("puts workspaces before core and others, when no relations", function()
+      local a = schema_new({ name = "a", fields = { { a = { type = "string" } } } })
+      local workspaces = schema_new({ name = "workspaces", fields = { { w = { type = "boolean" } } } })
+      local routes = schema_new({ name = "routes", fields = { { r = { type = "boolean" } } } })
+
+      local x = ts({ a, routes, workspaces })
+      assert.same({"workspaces", "routes", "a"},  collect_names(x))
+    end)
+
+    it("puts workspaces first, core entities second, and other entities afterwards, even with relations", function()
+      local a = schema_new({ name = "a", fields = { { a = { type = "string" } } } })
+      local services = schema_new({ name = "services", fields = {} })
+      local b = schema_new({
+        name = "b",
+        fields = {
+          { service = { type = "foreign", reference = "services" }, },
+          { a = { type = "foreign", reference = "a" }, },
+        }
+      })
+      local routes = schema_new({
+        name = "routes",
+        fields = {
+          { service = { type = "foreign", reference = "services" }, },
+        }
+      })
+      local workspaces = schema_new({
+        name = "workspaces",
+        fields = { { b = { type = "boolean" } } }
+      })
+      local x = ts({ services, b, a, workspaces, routes })
+      assert.same({ "workspaces", "services", "routes", "a", "b" },  collect_names(x))
+    end)
+
+    it("overrides core order if dependencies force it", function()
+      -- This scenario is here in case in the future we allow plugin entities to precede core entities
+      -- Not applicable today (kong 2.3.x) but maybe in future releases
+      local a = schema_new({ name = "a", fields = { { a = { type = "string" } } } })
+      local services = schema_new({ name = "services", fields = {
+        { a = { type = "foreign", reference = "a" } } -- we somehow forced services to depend on a
+      }})
+      local workspaces = schema_new({ name = "workspaces", fields = {
+        { a = { type = "foreign", reference = "a" } } -- we somehow forced workspaces to depend on a
+      } })
+
+      local x = ts({ services, a, workspaces })
+      assert.same({ "a", "workspaces", "services" },  collect_names(x))
+    end)
+
+    it("returns an error if cycles are found", function()
+      local a = schema_new({
+        name = "a",
+        fields = {
+          { a = { type = "string" }, },
+          { b = { type = "foreign", reference = "b" }, },
+        }
+      })
+      local b = schema_new({
+        name = "b",
+        fields = {
+          { b = { type = "boolean" }, },
+          { a = { type = "foreign", reference = "a" }, },
+        }
+      })
+      local x, err = ts({ a, b })
+      assert.is_nil(x)
+      assert.equals("Cycle detected, cannot sort topologically", err)
+    end)
   end)
 end)
